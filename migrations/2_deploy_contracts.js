9a10deeff6f00ce884320bd92098d61b25b0cb77
Luke Schoen <ltfschoen@gmail.com>
2018-01-17 11:08:31 +1100

    feat: Add Migration file to deploy specific contracts

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+const FixedSupplyToken = artifacts.require("./FixedSupplyToken.sol");
+const owned = artifacts.require("./owned.sol");
+const Exchange = artifacts.require("./Exchange.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(FixedSupplyToken);
+  deployer.deploy(owned);
+  deployer.deploy(Exchange);
+};
