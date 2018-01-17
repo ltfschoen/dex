e8bd16e55920612293c20cc10cace6329c752a4f
Luke Schoen <ltfschoen@gmail.com>
2018-01-17 11:30:31 +1100

    test: Add Tests to verify second account has no tokens initially and tokens transfer correctly

--- test/01_fixed_supply_token_test.js
+++ test/01_fixed_supply_token_test.js
@@ -15,4 +15,50 @@ contract('FixedSupplyToken', function(accounts) {
         assert.equal(balanceAccountOwner.toNumber(), _totalSupply.toNumber(), "Total Amount of tokens is owned by owner");
     });
   });
+
+  it("second account in TestRPC should own no tokens", function() {
+    var myTokenInstance;
+    return fixedSupplyToken.deployed().then(function(instance) {
+      myTokenInstance = instance;
+      return myTokenInstance.balanceOf(accounts[1]);
+    }).then(function(balanceAccountOwner) {
+      assert.equal(balanceAccountOwner.toNumber(), 0, "Total Amount of tokens is owned by some other address");
+    });
+  });
+
+  it("should send token correctly between accounts", function() {
+    var token;
+
+    // Get initial balances of first and second account
+    var account_one = accounts[0];
+    var account_two = accounts[1];
+
+    var account_one_starting_balance;
+    var account_two_starting_balance;
+    var account_one_ending_balance;
+    var account_two_ending_balance;
+
+    var amount = 10;
+
+    return fixedSupplyToken.deployed().then(function(instance) {
+      token = instance;
+      return token.balanceOf.call(account_one);
+    }).then(function(balance) {
+      account_one_starting_balance = balance.toNumber();
+      return token.balanceOf.call(account_two);
+    }).then(function(balance) {
+      account_two_starting_balance = balance.toNumber();
+      return token.transfer(account_two, amount, {from: account_one});
+    }).then(function() {
+      return token.balanceOf.call(account_one);
+    }).then(function(balance) {
+      account_one_ending_balance = balance.toNumber();
+      return token.balanceOf.call(account_two);
+    }).then(function(balance) {
+      account_two_ending_balance = balance.toNumber();
+
+      assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount was not correctly received from the sender");
+      assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount was not correctly sent to the receiver");
+    });
+  });
 });
