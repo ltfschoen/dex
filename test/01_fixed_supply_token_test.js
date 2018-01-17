c70b1ca922b13225f18105820730a9021a14c5a8
Luke Schoen <ltfschoen@gmail.com>
2018-01-17 14:16:44 +1100

    fix: Fix indentation in test file

--- test/01_fixed_supply_token_test.js
+++ test/01_fixed_supply_token_test.js
@@ -6,13 +6,13 @@ contract('FixedSupplyToken', function(accounts) {
     var _totalSupply;
     var myTokenInstance;
     return fixedSupplyToken.deployed().then(function(instance) {
-        myTokenInstance = instance;
-        return myTokenInstance.totalSupply.call();
+      myTokenInstance = instance;
+      return myTokenInstance.totalSupply.call();
     }).then(function(totalSupply) {
-        _totalSupply = totalSupply;
-        return myTokenInstance.balanceOf(accounts[0]);
+      _totalSupply = totalSupply;
+      return myTokenInstance.balanceOf(accounts[0]);
     }).then(function(balanceAccountOwner) {
-        assert.equal(balanceAccountOwner.toNumber(), _totalSupply.toNumber(), "Total Amount of tokens is owned by owner");
+      assert.equal(balanceAccountOwner.toNumber(), _totalSupply.toNumber(), "Total Amount of tokens is owned by owner");
     });
   });
 
